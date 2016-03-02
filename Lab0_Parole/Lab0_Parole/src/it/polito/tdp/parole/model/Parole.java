42314d81fb8817fe8f62575f8ea5f73e978fc972
jimmy-sonny <s209051@studenti.polito.it>
2016-03-02 11:17:21 +0100

    first commit

---
+++ Lab0_Parole/Lab0_Parole/src/it/polito/tdp/parole/model/Parole.java
@@ -0,0 +1,23 @@
+package it.polito.tdp.parole.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*********STUB CLASS*************/
+public class Parole {
+	
+	
+	public Parole() {
+	}
+	
+	public void addParola(String p) {
+	}
+	
+	public List<String> getElenco() {
+		return new ArrayList<String>() ;
+	}
+	
+	public void reset() {
+	}
+
+}
